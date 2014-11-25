4260457651c6af2407a00882f8797e59d5725ab4
Ryan Weaver <rw8334@ship.edu>
2014-11-25 14:31:02 -0500

    Just changed the first page to a simple login page, still need to change the password so it isn't visible

--- src/web/prog/client/GWT_POS.java
+++ src/web/prog/client/GWT_POS.java
@@ -39,23 +39,25 @@ public class GWT_POS implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		final Button sendButton = new Button("Send");
-		final TextBox nameField = new TextBox();
-		nameField.setText("GWT User");
+		final Button loginButton = new Button("Login");
+		final TextBox usernameField = new TextBox();
+		final TextBox passwordField = new TextBox();
+		//usernameField.setText("GWT User");
 		final Label errorLabel = new Label();
 
 		// We can add style names to widgets
-		sendButton.addStyleName("sendButton");
+		loginButton.addStyleName("sendButton");
 
 		// Add the nameField and sendButton to the RootPanel
 		// Use RootPanel.get() to get the entire body element
-		RootPanel.get("nameFieldContainer").add(nameField);
-		RootPanel.get("sendButtonContainer").add(sendButton);
+		RootPanel.get("usernameFieldContainer").add(usernameField);
+		RootPanel.get("passwordFieldContainer").add(passwordField);
+		RootPanel.get("loginButtonContainer").add(loginButton);
 		RootPanel.get("errorLabelContainer").add(errorLabel);
 
 		// Focus the cursor on the name field when the app loads
-		nameField.setFocus(true);
-		nameField.selectAll();
+		usernameField.setFocus(true);
+		usernameField.selectAll();
 
 		// Create the popup dialog box
 		final DialogBox dialogBox = new DialogBox();
@@ -80,8 +82,8 @@ public class GWT_POS implements EntryPoint {
 		closeButton.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
 				dialogBox.hide();
-				sendButton.setEnabled(true);
-				sendButton.setFocus(true);
+				loginButton.setEnabled(true);
+				loginButton.setFocus(true);
 			}
 		});
 
@@ -109,14 +111,14 @@ public class GWT_POS implements EntryPoint {
 			private void sendNameToServer() {
 				// First, we validate the input.
 				errorLabel.setText("");
-				String textToServer = nameField.getText();
+				String textToServer = usernameField.getText();
 				if (!FieldVerifier.isValidName(textToServer)) {
 					errorLabel.setText("Please enter at least four characters");
 					return;
 				}
 
 				// Then, we send the input to the server.
-				sendButton.setEnabled(false);
+				loginButton.setEnabled(false);
 				textToServerLabel.setText(textToServer);
 				serverResponseLabel.setText("");
 				greetingService.greetServer(textToServer,
@@ -146,7 +148,7 @@ public class GWT_POS implements EntryPoint {
 
 		// Add a handler to send the name to the server
 		MyHandler handler = new MyHandler();
-		sendButton.addClickHandler(handler);
-		nameField.addKeyUpHandler(handler);
+		loginButton.addClickHandler(handler);
+		usernameField.addKeyUpHandler(handler);
 	}
 }
