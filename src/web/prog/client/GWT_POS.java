6a51f96293c4fd8839c09625dc5c732e079102ef
Ryan Weaver <rw8334@ship.edu>
2014-12-03 00:18:15 -0500

    asd

--- src/web/prog/client/GWT_POS.java
+++ src/web/prog/client/GWT_POS.java
@@ -7,8 +7,6 @@ import com.google.gwt.dom.client.Document;
 import com.google.gwt.dom.client.Style.Visibility;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.PasswordTextBox;
@@ -20,75 +18,126 @@ import com.google.gwt.user.client.ui.TextBox;
  */
 public class GWT_POS implements EntryPoint {
 
+	final Button loginButton = new Button("Login");
+	final TextBox usernameField = new TextBox();
+	final PasswordTextBox passwordField = new PasswordTextBox();
+	final Label errorLabel = new Label();
+	final Button logoutButton = new Button("Logout");
+	
+	final Button createInvButton = new Button("Create Inventory Item");
+	final Button deleteInvButton = new Button("Delete Inventory Item");
+	final Button editInvButton = new Button("Edit Inventory Item");
+	final Button createCustRecButton = new Button("Create Customer Record");
+	final Button createPOSTranButton = new Button("Create POS Transaction");
+	final Button listTranButton = new Button("List Transactions");
+	
 	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		final Button loginButton = new Button("Login");
-		final TextBox usernameField = new TextBox();
-		final PasswordTextBox passwordField = new PasswordTextBox();
-		final Label errorLabel = new Label();
-		
-		final Button logoutButton = new Button("Logout");
 		logoutButton.setWidth("188px");
-		
 		passwordField.setWidth("176px");
 		passwordField.setHeight("18px");
 		loginButton.setWidth("188px");
 
-		// Add the nameField and sendButton to the RootPanel
-		// Use RootPanel.get() to get the entire body element
-		RootPanel.get("usernameFieldContainer").add(usernameField);
-		RootPanel.get("passwordFieldContainer").add(passwordField);
-		RootPanel.get("loginButtonContainer").add(loginButton);
-		RootPanel.get("errorLabelContainer").add(errorLabel);
-		RootPanel.get("logoutButtonContainer").add(logoutButton);
+		// Add login information to Root Panel
+		RootPanel.get("usernameField").add(usernameField);
+		RootPanel.get("passwordField").add(passwordField);
+		RootPanel.get("loginButton").add(loginButton);
+		RootPanel.get("errorLabel").add(errorLabel);
+		RootPanel.get("logoutButton").add(logoutButton);
+		
+		// Add menu information to Root Panel
+		RootPanel.get("createInvButton").add(createInvButton);
+		RootPanel.get("deleteInvButton").add(deleteInvButton);
+		RootPanel.get("editInvButton").add(editInvButton);
+		RootPanel.get("createCustRecButton").add(createCustRecButton);
+		RootPanel.get("createPOSTranButton").add(createPOSTranButton);
+		RootPanel.get("listTranButton").add(listTranButton);
 
 		// Focus the cursor on the name field when the app loads
 		usernameField.setFocus(true);
 		logoutButton.setVisible(false);
-
+		setMenuVisible(false);
+		
 		// Create a handler for the loginButton
-		class MyHandler implements ClickHandler, KeyUpHandler {
+		class LoginHandler implements ClickHandler {
 			/**
 			 * Fired when the user clicks on the loginButton.
 			 */
 			public void onClick(ClickEvent event) {
-				sendNameToServer();
+				login();
 			}
 
-		    /**
-			 * Fired when the user types in the usernameField.
-			 */
-			public void onKeyUp(KeyUpEvent event) {}
-
 			/**
-			 * Send the name from the nameField to the server and wait for a response.
+			 * Send the name from the nameField to the server and wait for a
+			 * response.
 			 */
-			private void sendNameToServer() {
+			private void login() {
+
 				// First, we validate the input.
 				errorLabel.setText("");
 				String username = usernameField.getText();
 				String password = passwordField.getText();
 				if (!LoginVerifier.areCredentialsGood(username, password)) {
-					errorLabel.setText("The username and/or password is not correct");
+					errorLabel
+							.setText("The username and/or password is not correct");
 					return;
-				}
-				else{
-					Document.get().getElementById("loginPage").getStyle().setVisibility(Visibility.HIDDEN);
+				} else {
+					hideAll();
+					setLoginVisible(false);
+					loginButton.setEnabled(false);
 					logoutButton.setVisible(true);
-				}
-
-				// Then, we send the input to the server.
-				loginButton.setEnabled(false);
-
+					setMenuVisible(true);
+				}				
 			}
-			
+
+		}
+		// Add a handler to send the name to the server
+		LoginHandler loginHandler = new LoginHandler();
+		loginButton.addClickHandler(loginHandler);
+
+		// Create a handler for the logoutButton
+		class LogoutHandler implements ClickHandler {
+
+			public void onClick(ClickEvent event) {
+				logout();
+			}
+
+			private void logout() {
+				hideAll();
+				setLoginVisible(true);
+				setMenuVisible(false);
+				loginButton.setEnabled(true);
+			}
 		}
 
-		// Add a handler to send the name to the server
-		MyHandler handler = new MyHandler();
-		loginButton.addClickHandler(handler);
-		usernameField.addKeyUpHandler(handler);
+		LogoutHandler logoutHandler = new LogoutHandler();
+		logoutButton.addClickHandler(logoutHandler);
+	}
+
+	private void hideAll() {
+		Document.get().getElementById("all").getStyle()
+				.setVisibility(Visibility.HIDDEN);
+	}
+
+	private void setLoginVisible(Boolean visible) {
+		if (visible == true) {
+			Document.get().getElementById("loginPage").getStyle()
+					.setVisibility(Visibility.VISIBLE);
+		} else {
+			Document.get().getElementById("loginPage").getStyle()
+					.setVisibility(Visibility.HIDDEN);
+		}
+	}
+	
+	private void setMenuVisible(Boolean visible){
+		if (visible == true) {
+			Document.get().getElementById("menu").getStyle()
+					.setVisibility(Visibility.VISIBLE);
+		} else {
+			Document.get().getElementById("menu").getStyle()
+					.setVisibility(Visibility.HIDDEN);
+		}
 	}
 }
