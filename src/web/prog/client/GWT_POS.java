83e6deee914bd3e4c8bfa4557cebfe3a71de2eaf
Ryan Weaver <rw8334@ship.edu>
2014-12-03 01:49:43 -0500

    Added new menu section with some display functionality to create inventory button

--- src/web/prog/client/GWT_POS.java
+++ src/web/prog/client/GWT_POS.java
@@ -4,6 +4,7 @@ import web.prog.shared.LoginVerifier;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.Style.Display;
 import com.google.gwt.dom.client.Style.Visibility;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
@@ -23,14 +24,14 @@ public class GWT_POS implements EntryPoint {
 	final PasswordTextBox passwordField = new PasswordTextBox();
 	final Label errorLabel = new Label();
 	final Button logoutButton = new Button("Logout");
-	
+
 	final Button createInvButton = new Button("Create Inventory Item");
 	final Button deleteInvButton = new Button("Delete Inventory Item");
 	final Button editInvButton = new Button("Edit Inventory Item");
 	final Button createCustRecButton = new Button("Create Customer Record");
 	final Button createPOSTranButton = new Button("Create POS Transaction");
 	final Button listTranButton = new Button("List Transactions");
-	
+
 	/**
 	 * This is the entry point method.
 	 */
@@ -40,13 +41,23 @@ public class GWT_POS implements EntryPoint {
 		passwordField.setHeight("18px");
 		loginButton.setWidth("188px");
 
+		usernameField.setText("username");
+		passwordField.setText("password");
+
+		createInvButton.setWidth("188px");
+		deleteInvButton.setWidth("188px");
+		editInvButton.setWidth("188px");
+		createCustRecButton.setWidth("188px");
+		createPOSTranButton.setWidth("188px");
+		listTranButton.setWidth("188px");
+
 		// Add login information to Root Panel
 		RootPanel.get("usernameField").add(usernameField);
 		RootPanel.get("passwordField").add(passwordField);
 		RootPanel.get("loginButton").add(loginButton);
 		RootPanel.get("errorLabel").add(errorLabel);
 		RootPanel.get("logoutButton").add(logoutButton);
-		
+
 		// Add menu information to Root Panel
 		RootPanel.get("createInvButton").add(createInvButton);
 		RootPanel.get("deleteInvButton").add(deleteInvButton);
@@ -57,25 +68,18 @@ public class GWT_POS implements EntryPoint {
 
 		// Focus the cursor on the name field when the app loads
 		usernameField.setFocus(true);
-		logoutButton.setVisible(false);
+		hideAll();
+
+		setLoginVisible(true);
+		setLogoutVisible(false);
 		setMenuVisible(false);
-		
+
 		// Create a handler for the loginButton
 		class LoginHandler implements ClickHandler {
 			/**
 			 * Fired when the user clicks on the loginButton.
 			 */
 			public void onClick(ClickEvent event) {
-				login();
-			}
-
-			/**
-			 * Send the name from the nameField to the server and wait for a
-			 * response.
-			 */
-			private void login() {
-
-				// First, we validate the input.
 				errorLabel.setText("");
 				String username = usernameField.getText();
 				String password = passwordField.getText();
@@ -85,15 +89,13 @@ public class GWT_POS implements EntryPoint {
 					return;
 				} else {
 					hideAll();
-					setLoginVisible(false);
-					loginButton.setEnabled(false);
-					logoutButton.setVisible(true);
+					setLogoutVisible(true);
 					setMenuVisible(true);
-				}				
+				}
 			}
-
 		}
-		// Add a handler to send the name to the server
+
+		// Add a handler to verify the username and password
 		LoginHandler loginHandler = new LoginHandler();
 		loginButton.addClickHandler(loginHandler);
 
@@ -101,42 +103,83 @@ public class GWT_POS implements EntryPoint {
 		class LogoutHandler implements ClickHandler {
 
 			public void onClick(ClickEvent event) {
-				logout();
-			}
-
-			private void logout() {
 				hideAll();
 				setLoginVisible(true);
 				setMenuVisible(false);
-				loginButton.setEnabled(true);
+				setLogoutVisible(false);
 			}
 		}
 
+		// Add a handler to log the user out
 		LogoutHandler logoutHandler = new LogoutHandler();
 		logoutButton.addClickHandler(logoutHandler);
+
+		class CreateInvButtonHandler implements ClickHandler {
+			int count = 0;
+
+			public void onClick(ClickEvent event) {
+				count++;
+
+				if (count % 2 == 1) {
+					createInvButton.setVisible(true);
+					Document.get().getElementById("deleteInvButton").getStyle().setDisplay(Display.NONE);
+					Document.get().getElementById("editInvButton").getStyle().setDisplay(Display.NONE);
+					Document.get().getElementById("createCustRecButton").getStyle().setDisplay(Display.NONE);
+					Document.get().getElementById("createPOSTranButton").getStyle().setDisplay(Display.NONE);
+					Document.get().getElementById("listTranButton").getStyle().setDisplay(Display.NONE);
+				} else {
+					createInvButton.setVisible(true);
+					Document.get().getElementById("deleteInvButton").getStyle().setDisplay(Display.BLOCK);
+					Document.get().getElementById("editInvButton").getStyle().setDisplay(Display.BLOCK);
+					Document.get().getElementById("createCustRecButton").getStyle().setDisplay(Display.BLOCK);
+					Document.get().getElementById("createPOSTranButton").getStyle().setDisplay(Display.BLOCK);
+					Document.get().getElementById("listTranButton").getStyle().setDisplay(Display.BLOCK);
+				}
+			}
+		}
+
+		// Add a handler to the Create Inventory Button
+		CreateInvButtonHandler createInvButtonHandler = new CreateInvButtonHandler();
+		createInvButton.addClickHandler(createInvButtonHandler);
 	}
 
 	private void hideAll() {
-		Document.get().getElementById("all").getStyle()
+		Document.get().getElementById("login").getStyle()
+				.setVisibility(Visibility.HIDDEN);
+
+		Document.get().getElementById("menu").getStyle()
+				.setVisibility(Visibility.HIDDEN);
+
+		Document.get().getElementById("logout").getStyle()
 				.setVisibility(Visibility.HIDDEN);
 	}
 
 	private void setLoginVisible(Boolean visible) {
 		if (visible == true) {
-			Document.get().getElementById("loginPage").getStyle()
+			Document.get().getElementById("login").getStyle()
 					.setVisibility(Visibility.VISIBLE);
-		} else {
-			Document.get().getElementById("loginPage").getStyle()
+		} else if (visible == false) {
+			Document.get().getElementById("login").getStyle()
 					.setVisibility(Visibility.HIDDEN);
 		}
 	}
-	
-	private void setMenuVisible(Boolean visible){
+
+	private void setMenuVisible(Boolean visible) {
 		if (visible == true) {
 			Document.get().getElementById("menu").getStyle()
 					.setVisibility(Visibility.VISIBLE);
-		} else {
+		} else if (visible == false) {
 			Document.get().getElementById("menu").getStyle()
+					.setVisibility(Visibility.HIDDEN);
+		}
+	}
+
+	private void setLogoutVisible(Boolean visible) {
+		if (visible == true) {
+			Document.get().getElementById("logout").getStyle()
+					.setVisibility(Visibility.VISIBLE);
+		} else if (visible == false) {
+			Document.get().getElementById("logout").getStyle()
 					.setVisibility(Visibility.HIDDEN);
 		}
 	}
